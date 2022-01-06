6dc0cec7107ac833bb95f7249510958c819d2f54
Rahul K <rahul.kudala@cjsstechnologies.com>
2022-01-06 18:46:11 +0530

    initializing on 06-01-2022

---
+++ Action_Service/src/main/java/com/example/Action_Service/ActionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.Action_Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActionServiceApplication.class, args);
+	}
+
+}
---
+++ Action_Service/src/test/java/com/example/Action_Service/ActionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Action_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
